3206c2efddd0fd6b21772de314fd5d34295a01c9
Pham Van Lap <quoclaptk@gmail.com>
2020-02-26 16:40:21 +0700

    Update01 for test python

--- git_test.py
+++ git_test.py
@@ -1,5 +1,21 @@
 import os
 
-print "Hello World!"
-for i in range(0, 1000):
-    print i
+#print "Hello World!"
+#for i in range(0, 1000):
+#    print i
+
+#******************************************
+arr_test = [1, 5, 7, 9, 3, 2, 0, 4]
+
+def sap_xep_tang_dan(arr):
+    for i in range(len(arr)-1):
+        for j in range(i+1, len(arr)):
+            temp = None
+            if arr[i] > arr[j]:
+               temp = arr[j];
+               arr[j] = arr[i]
+               arr[i] = temp;
+    print arr_test
+
+# call function
+sap_xep_tang_dan(arr_test)
